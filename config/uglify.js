cc5e3c6fe7aff215929d9fb975ccb1174de00060
Jae Sung Park <alberto.park@gmail.com>
2018-01-11 16:22:17 +0900

    skip: update uglify options

--- config/uglify.js
+++ config/uglify.js
@@ -1,17 +1,14 @@
 module.exports = {
 	include: /\.min\.js$/,
-	beautify: false,
-	mangle: {
-		screw_ie8: false,
-		keep_fnames: true
-	},
-	compress: {
-		screw_ie8: false,
-		warnings: false
-	},
-	output: {
-		screw_ie8: false
-	},
-	comments: false,
-	sourceMap: true
+	sourceMap: true,
+	uglifyOptions: {
+		beautify: false,
+		mangle: {
+			keep_fnames: true
+		},
+		compress: {
+			warnings: false
+		},
+		comments: false
+	}
 };
