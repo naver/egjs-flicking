e13b46204e02a8acbb1ee2013f9f4e6ecd801469
WoodNeck <saiyu915@naver.com>
2019-05-22 17:54:08 +0900

    demo: add non-infinite demos

---
+++ packages/vue-flicking/demo/features/Infinite.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Infinite extends Vue {
+}
+</script>
+<style scoped>
+
+</style>
