390db78a3e436d86f9c7d02fcd98b815924bbeb3
Daybrush <younkue.choi@navercorp.com>
2019-05-23 17:01:34 +0900

    fix: fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,38 +5,51 @@
 // flicking.pkgd.min.js
 // flicking.esm.js
 
+const buildHelper = require("@egjs/build-helper");
 
-const {esm, umds} = require("./config/bundler");
+const external = {
+	"@egjs/axes": "eg.Axes",
+	"@egjs/component": "eg.Component",
+}
+const name = "eg.Flicking";
 
-
-export default [
-	...umds({
-		input: "./src/index.umd.ts",
-		outputs: [
-			`./dist/flicking.js`,
-			`./dist/flicking.min.js`,
-		],
-		library: "eg.Flicking",
-		externals: {
-			"@egjs/axes": "eg.Axes",
-			"@egjs/component": "eg.Component",
-		},
-	}),
-	...umds({
-		input: "./src/index.umd.ts",
-		outputs: [
-			`./dist/flicking.pkgd.js`,
-			`./dist/flicking.pkgd.min.js`,
-		],
-		library: "eg.Flicking",
-	}),
-	esm({
-		input: "./src/index.ts",
-		output: "./dist/flicking.esm.js",
-		externals: {
-			"@egjs/axes": "eg.Axes",
-			"@egjs/component": "eg.Component",
-		},
-	}),
-];
+export default buildHelper([
+		{
+      name,
+			input: "./src/index.umd.ts",
+      output: "./dist/flicking.js",
+      format: "umd",
+      external,
+    },
+    {
+      name,
+			input: "./src/index.umd.ts",
+      output: "./dist/flicking.min.js",
+      format: "umd",
+      uglify: true,
+      external,
+    },
+    {
+      name,
+			input: "./src/index.umd.ts",
+      output: "./dist/flicking.pkgd.js",
+      format: "umd",
+      resolve: true,
+    },
+    {
+      name,
+			input: "./src/index.umd.ts",
+      output: "./dist/flicking.pkgd.min.js",
+      format: "umd",
+      resolve: true,
+      uglify: true,
+    },
+    {
+      input: "./src/index.ts",
+      output: "./dist/flicking.esm.js",
+      format: "esm",
+      external,
+      exports: "named",
+    },
+]);
 
--- src/index.umd.ts
+++ src/index.umd.ts
@@ -1,3 +1,8 @@
 import Flicking from "./Flicking";
+import { withFlickingMethods } from "./utils";
+import { DEFAULT_OPTIONS, MOVE_TYPE } from "./consts";
 
+(Flicking as any).withFlickingMethods = withFlickingMethods;
+(Flicking as any).DEFAULT_OPTIONS = DEFAULT_OPTIONS;
+(Flicking as any).MOVE_TYPE = MOVE_TYPE;
 export default Flicking;
