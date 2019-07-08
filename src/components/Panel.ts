88251eb2cc71e7fe72896ff05f5ba933b34e409e
WoodNeck <saiyu915@naver.com>
2019-07-08 19:00:28 +0900

    feat: show only visible panel elements

--- src/components/Panel.ts
+++ src/components/Panel.ts
@@ -238,14 +238,6 @@ class Panel implements FlickingPanel {
     return this;
   }
 
-  public show(): void {
-
-  }
-
-  public hide(): void {
-
-  }
-
   public destroy(option: Partial<DestroyOption>): void {
     if (!option.preserveUI) {
       const originalStyle = this.state.originalStyle;
