a3f2b5ebd8f87468cb7091f00749f3baead7517f
WoodNeck <saiyu915@naver.com>
2021-07-01 11:44:11 +0900

    feat: add ViewportSlot for react-flicking

---
+++ packages/react-flicking/src/react-flicking/ViewportSlot.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ViewportSlot: React.FC = ({ children }) => <>{children}</>;
+
+export default ViewportSlot;
--- packages/react-flicking/src/react-flicking/index.ts
+++ packages/react-flicking/src/react-flicking/index.ts
@@ -3,8 +3,12 @@
  * egjs projects are licensed under the MIT license
  */
 import Flicking from "./Flicking";
+import ViewportSlot from "./ViewportSlot";
 
 export * from "@egjs/flicking";
 export * from "./types";
 
+export {
+  ViewportSlot
+}
 export default Flicking;
