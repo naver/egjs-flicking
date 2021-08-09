3d4582d37e77c16ae6cd787327133bc63d037a9f
WoodNeck <saiyu915@naver.com>
2021-08-09 16:46:56 +0900

    test: add test cases for sync

---
+++ test/unit/cfc/sync.spec.ts
@@ -0,0 +1,87 @@
+import { DiffResult } from "@egjs/list-differ";
+import sync from "~/cfc/sync";
+
+import El from "../helper/El";
+import { createFlicking } from "../helper/test-util";
+
+describe("sync", () => {
+  it("can remove consecutive panels", async () => {
+    const flicking = await createFlicking(El.DEFAULT_HORIZONTAL);
+    const prevPanels = flicking.panels;
+    const diffResult: DiffResult<HTMLElement> = {
+      prevList: flicking.panels.map(panel => panel.element),
+      list: [flicking.panels[0].element],
+      added: [],
+      removed: [2, 1],
+      changed: [],
+      ordered: [],
+      pureChanged: [],
+      maintained: []
+    };
+
+    sync(flicking, diffResult, []);
+
+    expect(flicking.panels.length).to.equal(1);
+    expect(flicking.panels[0]).to.deep.equal(prevPanels[0]);
+  });
+
+  it("can remove the first panel", async () => {
+    const flicking = await createFlicking(El.DEFAULT_HORIZONTAL);
+    const prevPanels = [...flicking.panels];
+    const diffResult: DiffResult<HTMLElement> = {
+      prevList: flicking.panels.map(panel => panel.element),
+      list: flicking.panels.slice(1, 3).map(panel => panel.element),
+      added: [],
+      removed: [0],
+      changed: [],
+      ordered: [],
+      pureChanged: [],
+      maintained: []
+    };
+
+    sync(flicking, diffResult, []);
+
+    expect(flicking.panels.length).to.equal(2);
+    expect(flicking.panels).to.deep.equal(prevPanels.slice(1, 3));
+  });
+
+  it("can remove the last panel", async () => {
+    const flicking = await createFlicking(El.DEFAULT_HORIZONTAL);
+    const prevPanels = [...flicking.panels];
+    const diffResult: DiffResult<HTMLElement> = {
+      prevList: flicking.panels.map(panel => panel.element),
+      list: flicking.panels.slice(0, 2).map(panel => panel.element),
+      added: [],
+      removed: [2],
+      changed: [],
+      ordered: [],
+      pureChanged: [],
+      maintained: []
+    };
+
+    sync(flicking, diffResult, []);
+
+    expect(flicking.panels.length).to.equal(2);
+    expect(flicking.panels).to.deep.equal(prevPanels.slice(0, 2));
+  });
+
+  it("can remove non-consecutive panels", async () => {
+    const flicking = await createFlicking(El.DEFAULT_HORIZONTAL);
+    const prevPanels = [...flicking.panels];
+    const diffResult: DiffResult<HTMLElement> = {
+      prevList: flicking.panels.map(panel => panel.element),
+      list: [flicking.panels[1].element],
+      added: [],
+      removed: [0, 2],
+      changed: [],
+      ordered: [],
+      pureChanged: [],
+      maintained: []
+    };
+
+    sync(flicking, diffResult, []);
+
+    expect(flicking.panels.length).to.equal(1);
+    expect(flicking.panels).to.deep.equal([prevPanels[1]]);
+  });
+});
