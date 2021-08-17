442270f51bd1f934285064f0065ea471d4809301
WoodNeck <saiyu915@naver.com>
2021-08-17 20:03:28 +0900

    docs: add date selector demo

--- docs/src/component/code/Vue3Code.tsx
+++ docs/src/component/code/Vue3Code.tsx
@@ -4,7 +4,7 @@ import CodeBlock from "@theme/CodeBlock";
 import { SourceContext } from "./type";
 import { getClass, getImports, getOptionsObject, getPlugins, getStyle } from "./utils";
 
-export default ({ options, panels, plugins, siblings = [], imports = [] }: SourceContext) => {
+export default ({ options, panels, events = {}, methods = {}, plugins, siblings = [], imports = [] }: SourceContext) => {
   const optionsObject = getOptionsObject(options);
   const slots = panels.filter(panel => panel.isSlot);
   const declarePlugins = plugins ? `,
@@ -21,19 +21,27 @@ export default ({ options, panels, plugins, siblings = [], imports = [] }: Sourc
   </template>`
     : "";
   const allImports = [...(plugins ?? []).map(plugin => [`{ ${plugin[0]} }`, "@egjs/flicking-plugins"]), ...imports];
+  const methodKeys = Object.keys(methods);
+  const declareMethods = methodKeys.length > 0
+    ? `,
+  methods: {
+    ${methodKeys.map(key => `${key}: ${methods[key].split("\n").map((line, idx) => idx > 0  ? `    ${line}` : line).join("\n")}`).join(",\n")}
+  }`
+    : "";
+  const eventStatement = Object.keys(events).map(evt => ` @${evt.replace(/([A-Z])/g, "-$1").toLowerCase()}="${events[evt]}"`).join("");
 
   return <><CodeBlock className="html" title="template">
-    {`<Flicking${options ? ` :options="{ ${optionsObject} }"` : ""}${plugins ? " :plugins=\"plugins\"" : ""}>
+    {`<Flicking${options ? ` :options="{ ${optionsObject} }"` : ""}${plugins ? " :plugins=\"plugins\"" : ""}${eventStatement}>
   ${panels.filter(panel => !panel.isSlot).map(panel => `<${panel.tag}${getClass(panel)}${getStyle(panel)}>${panel.content}</${panel.tag}>`).join("\n  ")}${slotsTemplate}
 </Flicking>${siblings ? `\n${siblings.map(el => `<${el.tag}${getClass(el)}${getStyle(el)}>${el.content}</${el.tag}>`).join("\n")}` : ""}`}
   </CodeBlock>
   <CodeBlock className="js" title="script">
-    {`${getImports(allImports, { prefix: "vue" })}
+    {`${getImports(allImports, { prefix: "vue3" })}
 
 export default {
   components: {
     Flicking
-  }${declarePlugins}
+  }${declarePlugins}${declareMethods}
 }`}
   </CodeBlock>
   </>;
