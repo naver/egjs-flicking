5813fb4dadc2fb94d0d4fc26534d211e1a9be653
Mingyu Kim <saiyu915@naver.com>
2021-09-02 14:34:39 +0900

    fix: getRenderingPanels uses old list
    * fix: getRenderingPanels uses old list
    
    * test: add some test cases for #538

--- src/cfc/getRenderingPanels.ts
+++ src/cfc/getRenderingPanels.ts
@@ -8,12 +8,17 @@ export default <T>(flicking: Flicking, diffResult: DiffResult<T>) => {
     return map;
   }, {});
 
+  const maintainedMap = diffResult.maintained.reduce((map, [prev, current]) => {
+    map[prev] = current;
+    return map;
+  }, {});
+
   return [
     ...flicking.panels
       .filter(panel => !removedPanels[panel.index])
       // Sort panels by position
       .sort((panel1, panel2) => (panel1.position + panel1.offset) - (panel2.position + panel2.offset))
-      .map(panel => diffResult.prevList[panel.index]),
+      .map(panel => diffResult.list[maintainedMap[panel.index]]),
     ...diffResult.added.map(idx => diffResult.list[idx])
   ];
 };
