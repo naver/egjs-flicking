52652d8a05d2b27aebe7e7aba63c57a2bfc654ff
WoodNeck <saiyu915@naver.com>
2021-10-25 16:01:41 +0900

    test: fix TC of autoResize

--- test/manual/js/index.js
+++ test/manual/js/index.js
@@ -2,8 +2,10 @@ const flicking = new Flicking("#flicking", {
   circular: true,
   moveType: "strict",
   align: "prev"
+}).on("afterResize", () => {
+  console.log("resized");
 }).on("ready", () => {
-  console.log(flicking.currentPanel)
+  console.log("ready");
 });
 
 // Object.values(Flicking.EVENTS).forEach(evt => {
@@ -12,6 +14,7 @@ const flicking = new Flicking("#flicking", {
 
 // document.querySelector("#prev").addEventListener("click", () => flicking.prev().catch(() => void 0));
 // document.querySelector("#next").addEventListener("click", () => flicking.next().catch(() => void 0));
+document.querySelector("#resize").addEventListener("click", () => flicking.viewport.setSize({ width: 100, height: 100 }));
 
 // flicking.panels.forEach(panel => {
 //   panel.element.addEventListener("click", e => {
