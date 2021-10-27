2a6f460eb512ab95888e559eb7fd44703d6247a4
WoodNeck <saiyu915@naver.com>
2021-10-27 19:35:40 +0900

    chore(release): release packages

--- docs/src/component/EventFlicking.tsx
+++ docs/src/component/EventFlicking.tsx
@@ -1,32 +1,44 @@
 import React, { useRef, useState } from "react";
-import Flicking, { EVENTS } from "@egjs/react-flicking";
+import Flicking, { ViewportSlot } from "@egjs/react-flicking";
 
-export default ({ children, ...others }) => {
+export default ({ children, events, ...others }) => {
   const flicking = useRef<Flicking>();
-  const [events, setEvents] = useState<string[]>([]);
+  const [loggedEvents, setEvents] = useState<Array<{ eventType: string; count: number }>>([]);
 
-  let lastEvt = EVENTS.READY;
+  let lastEvt = null;
+
+  const evtHandlers = events.reduce((handlers, evtName) => {
+    const reactHandlerName = `on${evtName[0].toUpperCase()}${evtName.slice(1)}`;
+
+    handlers[reactHandlerName] = evt => {
+      if (lastEvt !== evt.eventType) {
+        setEvents((evts) => [...evts, { eventType: evt.eventType, count: 1 }]);
+      } else {
+        setEvents((evts) => [...evts.slice(0, evts.length - 1), { eventType: evts[evts.length - 1].eventType, count: evts[evts.length - 1].count + 1 }]);
+      }
+
+      lastEvt = evt.eventType;
+    };
+
+    return handlers;
+  }, {});
 
   return <div>
-    <Flicking ref={flicking} {...others} onReady={() => {
-      Object.keys(EVENTS).forEach(key => {
-        const eventName = EVENTS[key];
-
-        flicking.current.on(eventName, evt => {
-          if (evt.eventType === EVENTS.HOLD_START) {
-            setEvents(() => [evt.eventType]);
-          } else if (lastEvt !== evt.eventType) {
-            setEvents((evts) => [...evts, evt.eventType]);
-          }
-
-          lastEvt = evt.eventType;
-        });
-      });
-    }}>
+    <Flicking ref={flicking} {...evtHandlers} {...others}>
       { children }
+      <ViewportSlot>
+        <div className="field is-grouped is-grouped-multiline">
+          {
+            loggedEvents.map((evt, idx) => <div className="control mr-2 mb-1" key={idx}>
+              <div className="bulma-tags has-addons">
+                <span className="bulma-tag is-info">{evt.eventType}</span>
+                <span className="bulma-tag is-dark">{evt.count}</span>
+              </div>
+            </div>)
+          }
+        </div>
+        <div></div>
+      </ViewportSlot>
     </Flicking>
-    <div>{
-      events.map((evt, idx) => <span className="bulma-tag mr-2 is-info" key={idx}>{evt}</span>)
-    }</div>
   </div>;
 };
