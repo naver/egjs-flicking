8f40cf01c8682c27b887d0bef10b3f8f8603a2ea
WoodNeck <saiyu915@naver.com>
2021-11-12 17:09:29 +0900

    fix: typing error due to the default value

--- packages/vue3-flicking/src/FlickingProps.ts
+++ packages/vue3-flicking/src/FlickingProps.ts
@@ -14,10 +14,10 @@ class FlickingProps {
   viewportTag = prop<string>({ required: false, default: "div" });
   cameraTag = prop<string>({ required: false, default: "div" });
   hideBeforeInit = prop<boolean>({ required: false, default: false });
-  firstPanelSize = prop<string>({ required: false });
+  firstPanelSize = prop<string>({ required: false, default: null });
   options = prop<Partial<FlickingOptions>>({ required: false, default: {} });
   plugins = prop<Plugin[]>({ required: false, default: [] });
-  status = prop<Status>({ required: false });
+  status = prop<Status>({ required: false, default: null });
 }
 
 export default FlickingProps;
