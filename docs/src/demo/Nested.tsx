e8bc48d32301f7ce253c6bfef11acefe66553be1
malangfox <ahn3407@naver.com>
2022-05-11 07:26:44 +0900

    docs: update nested demo

--- docs/src/demo/Nested.tsx
+++ docs/src/demo/Nested.tsx
@@ -10,37 +10,24 @@ import Panel from "../component/Panel";
 
 export default () => {
   const vanillaHTML = `<div id="flick1" class="flicking-viewport">
+  <div className="flicking-panel">1</div>
   <div className="flicking-panel nested-wide">
     <div id="flick2" class="flicking-viewport">
-      <div className="flicking-panel nested-wide">
-        <div id="flick5" class="flicking-viewport vertical">
-          <div className="flicking-panel">1.09</div>
-          <div className="flicking-panel">1.1</div>
-          <div className="flicking-panel">1.11</div>
-        </div>
-      </div>
-      <div className="flicking-panel">1.2</div>
-      <div className="flicking-panel">1.3</div>
-    </div>
-  </div>
-  <div className="flicking-panel nested-wide">
-    <div id="flick3" class="flicking-viewport">
       <div className="flicking-panel">2.1</div>
       <div className="flicking-panel">2.2</div>
       <div className="flicking-panel">2.3</div>
     </div>
   </div>
-  <div className="flicking-panel nested-wide">
-    <div id="flick4" class="flicking-viewport">
+  <div className="flicking-panel nested-wide vertical">
+    <div id="flick3" class="flicking-viewport vertical">
       <div className="flicking-panel">3.1</div>
       <div className="flicking-panel">3.2</div>
       <div className="flicking-panel">3.3</div>
     </div>
   </div>
+  <div className="flicking-panel">4</div>
 </div>`;
-  const vanillaJS = `const flicking1 = new Flicking("#flick1", {
-  circular: true
-});
+  const vanillaJS = `const flicking1 = new Flicking("#flick1");
 const flicking2 = new Flicking("#flick2", {
   bounce: 0,
   bound: true,
@@ -49,36 +36,12 @@ const flicking2 = new Flicking("#flick2", {
 const flicking3 = new Flicking("#flick3", {
   bounce: 0,
   bound: true,
-  nested: true
-});
-const flicking4 = new Flicking("#flick4", {
-  bounce: 0,
-  bound: true,
-  nested: true
-});
-const flicking5 = new Flicking("#flick5", {
-  bounce: 0,
-  bound: true,
-  horizontal: false,
-  defaultIndex: 1,
-  circular: true
+  horizontal: false
 });`;
   const reactSourceCode = `export default () => {
     return <>
-      <Flicking circular={true}>
-        <div className="flicking-panel nested-wide">
-          <Flicking bounce="0" bound={true} nested={true}>
-            <div className="flicking-panel nested-wide">
-              <Flicking bounce="0" bound={true} horizontal={false} defaultIndex={1} circular={true}>
-                <div className="flicking-panel">1.09</div>
-                <div className="flicking-panel">1.1</div>
-                <div className="flicking-panel">1.11</div>
-              </Flicking>
-            </div>
-            <div className="flicking-panel">1.2</div>
-            <div className="flicking-panel">1.3</div>
-          </Flicking>
-        </div>
+      <Flicking>
+        <div className="flicking-panel">1</div>
         <div className="flicking-panel nested-wide">
           <Flicking bounce="0" bound={true} nested={true}>
             <div className="flicking-panel">2.1</div>
@@ -86,59 +49,37 @@ const flicking5 = new Flicking("#flick5", {
             <div className="flicking-panel">2.3</div>
           </Flicking>
         </div>
-        <div className="flicking-panel nested-wide">
-          <Flicking bounce="0" bound={true} nested={true}>
+        <div className="flicking-panel nested-wide vertical">
+          <Flicking bounce="0" bound={true} horizontal={false}>
             <div className="flicking-panel">3.1</div>
             <div className="flicking-panel">3.2</div>
             <div className="flicking-panel">3.3</div>
           </Flicking>
         </div>
+        <div className="flicking-panel">4</div>
       </Flicking>
     </>
   };`;
-  const angularSourceCode = `<ngx-flicking [options]="{ circular: true }">
-  <div flicking-panel class="nested-wide">
-    <ngx-flicking [options]="{ bounce: 0, bound: true, nested: true }">
-      <div flicking-panel class="nested-wide">
-        <ngx-flicking [options]="{ bounce: 0, bound: true, horizontal: false, defaultIndex: 1, circular: true }">
-          <div flicking-panel>1.09</div>
-          <div flicking-panel>1.1</div>
-          <div flicking-panel>1.11</div>
-        </div>
-      </div>
-      <div flicking-panel>1.2</div>
-      <div flicking-panel>1.3</div>
-    </div>
-  </div>
+  const angularSourceCode = `<ngx-flicking>
+  <div flicking-panel>1</div>
   <div flicking-panel class="nested-wide">
     <ngx-flicking [options]="{ bounce: 0, bound: true, nested: true }">
       <div flicking-panel>2.1</div>
       <div flicking-panel>2.2</div>
       <div flicking-panel>2.3</div>
-    </div>
+    </ngx-flicking>
   </div>
-  <div flicking-panel class="nested-wide">
-    <ngx-flicking [options]="{ bounce: 0, bound: true, nested: true }">
+  <div flicking-panel class="nested-wide vertical">
+    <ngx-flicking [options]="{ bounce: 0, bound: true, horizontal: false }">
       <div flicking-panel>3.1</div>
       <div flicking-panel>3.2</div>
       <div flicking-panel>3.3</div>
-    </div>
+    </ngx-flicking>
   </div>
+  <div flicking-panel>4</div>
 </div>`;
-  const vueSourceCode = `<flicking :options="{ circular: true }">
-  <div class="flicking-panel nested-wide">
-    <flicking :options="{ bounce: 0, bound: true, nested: true }">
-      <div class="flicking-panel nested-wide">
-        <flicking :options="{ bounce: 0, bound: true, horizontal: false, defaultIndex: 1, circular: true }">
-          <div class="flicking-panel">1.09</div>
-          <div class="flicking-panel">1.1</div>
-          <div class="flicking-panel">1.11</div>
-        </flicking>
-      </div>
-      <div class="flicking-panel">1.2</div>
-      <div class="flicking-panel">1.3</div>
-    </flicking>
-  </div>
+  const vueSourceCode = `<flicking>
+  <div class="flicking-panel">1</div>
   <div class="flicking-panel nested-wide">
     <flicking :options="{ bounce: 0, bound: true, nested: true }">
       <div class="flicking-panel">2.1</div>
@@ -146,32 +87,21 @@ const flicking5 = new Flicking("#flick5", {
       <div class="flicking-panel">2.3</div>
     </flicking>
   </div>
-  <div class="flicking-panel nested-wide">
-    <flicking :options="{ bounce: 0, bound: true, nested: true }">
+  <div class="flicking-panel nested-wide vertical">
+    <flicking :options="{ bounce: 0, bound: true, horizontal: false }">
       <div class="flicking-panel">3.1</div>
       <div class="flicking-panel">3.2</div>
       <div class="flicking-panel">3.3</div>
     </flicking>
   </div>
+  <div class="flicking-panel">4</div>
 </flicking>`;
   const svelteSourceCode = `<script lang="ts">
 import Flicking, { FlickingPanel } from "@egjs/svelte-flicking";
 </script>
 
-<Flicking options={{ circular: true }}>
-  <FlickingPanel class="nested-wide">
-    <Flicking options={{ bounce: 0, bound: true, nested: true }}>
-      <FlickingPanel class="nested-wide">
-        <Flicking options={{ bounce: 0, bound: true, horizontal: false, defaultIndex: 1, circular: true }}>
-          <FlickingPanel>1.09</FlickingPanel>
-          <FlickingPanel>1.1</FlickingPanel>
-          <FlickingPanel>1.11</FlickingPanel>
-        </Flicking>
-      </FlickingPanel>
-      <FlickingPanel>1.2</FlickingPanel>
-      <FlickingPanel>1.3</FlickingPanel>
-    </Flicking>
-  </FlickingPanel>
+<Flicking>
+  <FlickingPanel>1</FlickingPanel>
   <FlickingPanel class="nested-wide">
     <Flicking options={{ bounce: 0, bound: true, nested: true }}>
       <FlickingPanel>2.1</FlickingPanel>
@@ -179,30 +109,19 @@ import Flicking, { FlickingPanel } from "@egjs/svelte-flicking";
       <FlickingPanel>2.3</FlickingPanel>
     </Flicking>
   </FlickingPanel>
-  <FlickingPanel class="nested-wide">
-    <Flicking options={{ bounce: 0, bound: true, nested: true }}>
+  <FlickingPanel class="nested-wide vertical">
+    <Flicking options={{ bounce: 0, bound: true, horizontal: false }}>
       <FlickingPanel>3.1</FlickingPanel>
       <FlickingPanel>3.2</FlickingPanel>
       <FlickingPanel>3.3</FlickingPanel>
     </Flicking>
   </FlickingPanel>
+  <FlickingPanel>4</FlickingPanel>
 </Flicking>`;
 
   return <>
-    <Flicking className="py-4 mb-4" circular={true}>
-      <div className="flicking-panel nested-wide has-background-primary-dark">
-        <Flicking bounce="0" bound={true} nested={true}>
-          <div className="flicking-panel nested-wide">
-            <Flicking bounce="0" bound={true} horizontal={false} style={{ height: "140px" }} defaultIndex={1} circular={true}>
-              <Panel index={0.09} />
-              <Panel index={0.1} />
-              <Panel index={0.11} />
-            </Flicking>
-          </div>
-          <Panel index={0.2} />
-          <Panel index={0.3} />
-        </Flicking>
-      </div>
+    <Flicking className="py-4 mb-4">
+      <Panel index={0} />
       <div className="flicking-panel nested-wide has-background-primary-dark">
         <Flicking bounce="0" bound={true} nested={true}>
           <Panel index={1.1} />
@@ -210,13 +129,14 @@ import Flicking, { FlickingPanel } from "@egjs/svelte-flicking";
           <Panel index={1.3} />
         </Flicking>
       </div>
-      <div className="flicking-panel nested-wide has-background-primary-dark">
-        <Flicking bounce="0" bound={true} nested={true}>
+      <div className="flicking-panel nested-wide vertical has-background-primary-dark">
+        <Flicking bounce="0" bound={true} horizontal={false} style={{ height: "180px" }}>
           <Panel index={2.1} />
           <Panel index={2.2} />
           <Panel index={2.3} />
         </Flicking>
       </div>
+      <Panel index={3} />
     </Flicking>
     <SourceCode
       options={{ circular: true }} panels={[]}
