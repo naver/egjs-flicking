ef9590886043d52a0748da3bcd0c00af52ccb301
WoodNeck <saiyu915@naver.com>
2022-05-30 11:22:12 +0900

    test: changeOnHold

--- test/manual/js/index.js
+++ test/manual/js/index.js
@@ -1,5 +1,29 @@
 const flicking = new Flicking("#flicking", {
   moveType: "strict",
   circular: true,
-  align: "prev"
+  adaptive: true,
+  changeOnHold: true,
+  moveType: "strict"
 });
+
+flicking.on("willChange", evt => {
+  console.log("willChange", evt.index);
+});
+
+flicking.on("willRestore", evt => {
+  console.log("willRestore", evt.index);
+});
+
+flicking.on("changed", evt => {
+  console.log("changed", evt.index);
+});
+
+flicking.on("restored", evt => {
+  console.log("restored", evt.index);
+});
+
+// window.addEventListener("touchstart", () => {
+//   window.addEventListener("touchmove", evt => {
+//     console.log("touchmove", evt.cancelable);
+//   });
+// });
