aabb9fcdc030e32ec27e6c224b35106936dfceff
malangfox <ahn3407@naver.com>
2022-07-07 16:39:52 +0900

    fix: use referencePanel height when vertical

--- src/core/panel/Panel.ts
+++ src/core/panel/Panel.ts
@@ -308,7 +308,7 @@ class Panel {
    */
   public resize(cached?: {
     size: number;
-    height: number;
+    height?: number;
     margin: { prev: number; next: number };
   }): this {
     const el = this.element;
@@ -321,13 +321,13 @@ class Panel {
     if (cached) {
       this._size = cached.size;
       this._margin = { ...cached.margin };
-      this._height = horizontal ? getElementSize({
+      this._height = cached.height ? cached.height : getElementSize({
         el,
         horizontal: false,
         useFractionalSize,
         useOffset: true,
         style: getStyle(el)
-      }) : cached.height;
+      });
     } else {
       const elStyle = getStyle(el);
 
