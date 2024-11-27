6267ae0c3c3f220446df882b44d0755621a72f7c
malangfox <ahn3407@naver.com>
2024-11-28 05:43:25 +0900

    fix: apply element property to CrossGroup

--- packages/react-flicking/src/react-flicking/CrossGroup.tsx
+++ packages/react-flicking/src/react-flicking/CrossGroup.tsx
@@ -2,8 +2,18 @@
  * Copyright (c) 2015 NAVER Corp.
  * egjs projects are licensed under the MIT license
  */
-import React, { ReactNode } from "react";
+import React, { ReactNode, HTMLAttributes } from "react";
 
-const CrossGroup = React.forwardRef((props: { children?: ReactNode }, ref: React.Ref<HTMLDivElement>) => <div ref={ref}>{props.children}</div>);
+interface CrossGroupProps extends HTMLAttributes<HTMLDivElement> {
+  children?: ReactNode;
+}
+
+const CrossGroup = React.forwardRef<HTMLDivElement, CrossGroupProps>(
+  ({ children, ...rest }, ref) => (
+    <div ref={ref} {...rest}>
+      {children}
+    </div>
+  )
+);
 
 export default CrossGroup;
