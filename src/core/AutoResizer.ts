56618961b101f026ad67ddc90cbc3343007ede74
cjw783 <cjw783@gmail.com>
2025-05-16 16:00:01 +0900

    chore: add comments

--- src/core/AutoResizer.ts
+++ src/core/AutoResizer.ts
@@ -122,13 +122,16 @@ class AutoResizer {
     const resizeDebounce = flicking.resizeDebounce;
     const maxResizeDebounce = flicking.maxResizeDebounce;
 
-    const resizedViewportElement = entries.find(e => e.target === flicking.element)?.target;
-
+    const resizedViewportElement = flicking.element;
     // 현재 구현에서 리사이즈 옵저빙 대상은 패널과 뷰포트 2개만 존재.
-    // 아래는 뷰포트만 변경되었을 때 동작해야하는 로직이므로 아래와 같이 조건문을 건다.
-    // resizeObserver를 사용하지 않은 경우에는 entries.length가 0으로 오므로 이것도 고려한다.
-    // 패널 쪽에서는 리사이즈 감지에 resizeObserver를 사용하지 않는 경우가 없으므로 아래의 조건은 곧 뷰포트만 리사이즈가 된 경우를 의미한다.
-    if ((entries.length === 1 && resizedViewportElement) || entries.length === 0) {
+    // 아래는 뷰포트만 변경되었을 때 동작해야하는 로직이 있으므로 아래와 같이 조건문을 건다.
+    // 패널 쪽에서는 리사이즈 감지에 resizeObserver를 사용하지 않는 경우가 없으므로 이 조건은 곧 뷰포트만 리사이즈가 된 경우를 의미한다.
+    const isResizedViewportOnly = entries.find(e => e.target === flicking.element) && entries.length === 1;
+
+    // 참고: resizeObserver를 사용하지 않은 경우에는 entries.length가 0으로 오는데 이 경우에는 그냥 항상 리사이즈가 진행되도록 한다.
+    //   (vw, vh 등을 사용하는 경우 이상 동작이 발생할 여지가 있기 때문이다)
+    if (isResizedViewportOnly) {
+      // resize 이벤트가 발생했으나 이전과 width, height의 변화가 없다면 이후 로직을 진행하지 않는다.
       const beforeSize = {
         width: flicking.viewport.width,
         height: flicking.viewport.height
@@ -136,22 +139,21 @@ class AutoResizer {
 
       const afterSize = {
         width: getElementSize({
-          el: resizedViewportElement as HTMLElement,
+          el: resizedViewportElement,
           horizontal: true,
           useFractionalSize: this._flicking.useFractionalSize,
           useOffset: false,
-          style: getStyle(resizedViewportElement as HTMLElement)
+          style: getStyle(resizedViewportElement)
         }),
         height: getElementSize({
-          el: resizedViewportElement as HTMLElement,
+          el: resizedViewportElement,
           horizontal: false,
           useFractionalSize: this._flicking.useFractionalSize,
           useOffset: false,
-          style: getStyle(resizedViewportElement as HTMLElement)
+          style: getStyle(resizedViewportElement)
         })
       };
 
-      // resize 이벤트가 발생했으나 이전과 width, height의 변화가 없다면 이후 로직을 진행하지 않는다.
       if (
         beforeSize.height === afterSize.height &&
         beforeSize.width === afterSize.width
