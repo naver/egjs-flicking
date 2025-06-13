9832cf9f94d9138fb00c1d69d1b633febc5ad22c
cjw783 <cjw783@gmail.com>
2025-06-13 17:36:37 +0900

    test: fix failed test

--- test/unit/state-api/state-api.spec.ts
+++ test/unit/state-api/state-api.spec.ts
@@ -1,77 +1,81 @@
 import { expect } from "chai";
-import Flicking from "../../../src/Flicking";
-import { connectFlickingStateApi } from "../../../src/state-api";
+import Flicking from "~/Flicking";
+import { connectFlickingStateApi } from "~/state-api";
+import { cleanup, createFlicking, createSandbox, tick } from "../helper/test-util";
+import El from "../helper/El";
 
 describe("State API", () => {
+  let container: HTMLElement;
   let flicking: Flicking;
   let stateApi: ReturnType<typeof connectFlickingStateApi>;
 
-  beforeEach(() => {
-    const el = document.createElement("div");
-    el.innerHTML = `
-      <div class="flicking-viewport">
-        <div class="flicking-camera">
-          <div class="flicking-panel">1</div>
-          <div class="flicking-panel">2</div>
-          <div class="flicking-panel">3</div>
-        </div>
-      </div>
-    `;
-    document.body.appendChild(el);
-
-    flicking = new Flicking(".flicking-viewport");
+  beforeEach(async () => {
+    container = createSandbox("state-api-test");
+    flicking = await createFlicking(El.DEFAULT_HORIZONTAL);
     stateApi = connectFlickingStateApi(flicking);
   });
 
-  afterEach(() => {
-    document.body.innerHTML = "";
-    flicking.destroy();
+  afterEach(async () => {
+    cleanup();
+    await stateApi.unsubscribe();
   });
 
-  it("should initialize with correct values", () => {
-    expect(stateApi.isReachStart).to.be.true;
-    expect(stateApi.isReachEnd).to.be.false;
-    expect(stateApi.totalPanelCount).to.equal(3);
-    expect(stateApi.currentPanelIndex).to.equal(0);
-    expect(stateApi.progress).to.equal(0);
+  describe("Initial State", () => {
+    it("should initialize with correct values", () => {
+      expect(stateApi.isReachStart).to.be.true;
+      expect(stateApi.isReachEnd).to.be.false;
+      expect(stateApi.totalPanelCount).to.equal(3);
+      expect(stateApi.currentPanelIndex).to.equal(0);
+      expect(stateApi.progress).to.equal(0);
+    });
   });
 
-  it("should update state when moving to next panel", async () => {
-    await flicking.next();
+  describe("State Updates", () => {
+    it("should update state when moving to next panel", async () => {
+      const promise =  flicking.next();
+      tick(1000);
+      await promise;
 
-    expect(stateApi.isReachStart).to.be.false;
-    expect(stateApi.isReachEnd).to.be.false;
-    expect(stateApi.currentPanelIndex).to.equal(1);
-    expect(stateApi.progress).to.equal(50);
+      expect(stateApi.isReachStart).to.be.false;
+      expect(stateApi.isReachEnd).to.be.false;
+      expect(stateApi.currentPanelIndex).to.equal(1);
+      expect(stateApi.progress).to.equal(50);
+    });
+
+    it("should update state when moving to last panel", async () => {
+      const promise = flicking.moveTo(2);
+      tick(1000);
+      await promise;
+
+      expect(stateApi.isReachStart).to.be.false;
+      expect(stateApi.isReachEnd).to.be.true;
+      expect(stateApi.currentPanelIndex).to.equal(2);
+      expect(stateApi.progress).to.equal(100);
+    });
+
+    it("should update state when adding panels", async () => {
+      await flicking.append("<div class='flicking-panel'>4</div>");
+
+      expect(stateApi.totalPanelCount).to.equal(4);
+      expect(stateApi.isReachEnd).to.be.false;
+    });
+
+    it("should update state when removing panels", async () => {
+      await flicking.remove(1);
+
+      expect(stateApi.totalPanelCount).to.equal(2);
+      expect(stateApi.isReachEnd).to.be.false;
+    });
   });
 
-  it("should update state when moving to last panel", async () => {
-    await flicking.moveTo(2);
+  describe("moveTo Method", () => {
+    it("should move to specified index when moveTo is called", async () => {
+      const promise = stateApi.moveTo(1);
+      tick(1000);
+      await promise;
 
-    expect(stateApi.isReachStart).to.be.false;
-    expect(stateApi.isReachEnd).to.be.true;
-    expect(stateApi.currentPanelIndex).to.equal(2);
-    expect(stateApi.progress).to.equal(100);
-  });
-
-  it("should update state when adding panels", () => {
-    flicking.append("<div class='flicking-panel'>4</div>");
-
-    expect(stateApi.totalPanelCount).to.equal(4);
-    expect(stateApi.isReachEnd).to.be.false;
-  });
-
-  it("should update state when removing panels", () => {
-    flicking.remove(2);
-
-    expect(stateApi.totalPanelCount).to.equal(2);
-    expect(stateApi.isReachEnd).to.be.true;
-  });
-
-  it("should move to specified index when moveTo is called", async () => {
-    await stateApi.moveTo(1);
-
-    expect(stateApi.currentPanelIndex).to.equal(1);
-    expect(stateApi.progress).to.equal(50);
+      expect(stateApi.currentPanelIndex).to.equal(1);
+      expect(stateApi.progress).to.equal(50);
+    });
   });
 });
