737c1a8d020ede129da12b77f9db5aa32e83e736
malangfox <seonghyun.ahn@navercorp.com>
2025-07-17 15:22:19 +0900

    fix: fix error cases in ReactElementProvider

--- packages/react-flicking/src/react-flicking/ReactElementProvider.ts
+++ packages/react-flicking/src/react-flicking/ReactElementProvider.ts
@@ -10,10 +10,10 @@ class ReactElementProvider implements ElementProvider {
   private _elRef: React.RefObject<HTMLElement>;
 
   public get element() { return this._elRef.current!; }
-  public get rendered() { return this._elRef.current !== null; }
+  public get rendered() { return this._elRef.current != null; }
 
   public constructor(el: StrictPanel | NonStrictPanel | HTMLDivElement) {
-    this._elRef = el instanceof HTMLDivElement ? { current: el } : el.elRef;
+    this._elRef = el instanceof Element ? { current: el } : el.elRef;
   }
 
   public show() {
